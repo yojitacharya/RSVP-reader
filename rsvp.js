eb16222506291e6654f07aec4cec61df5e59e31c
yogipoo <151233809+yojitacharya@users.noreply.github.com>
2024-01-08 21:59:59 -0600

    update

--- rsvp.js
+++ rsvp.js
@@ -1,31 +1,55 @@
 var speed;
-var time = 0;
-var start = true;
+var startB = true;
+var text = [];
+var textString = "";
+var speedMS = 0;
+var IntervalID;
+var currentWordIndex = 0;
 
 window.onload = function() {
-    setInterval(addTime,0.5)
-    var text = [];
     document.getElementById("start").addEventListener("click", start);
 }
 
-function addTime() {
-    time += 1;
-}
-
 function start() {
-    time = 0;
-    speed = document.getElementById("speed").value;
-    document.getElementById("input").innerHTML = text;
 
+    if(startB == true) {
+        
+        startB = false;
 
-    if(start == true) {
-        start = false;
+        speed = document.getElementById("speed").value;
+
+        textString = document.getElementById("insert").value;
+        text = textString.split(" ");
+        console.log(text);
+
         document.getElementById("start").innerText = "Stop";
+
+        speedMS = 60000/speed;
+
+    IntervalID = setInterval(changeF, speedMS);
+
     }
 
     else {
-        start = true;
+        startB = true;
         document.getElementById("start").innerText = "Start";
+
+        clearInterval(IntervalID);
+
+        currentWordIndex = 0;
+
+        document.getElementById("change").innerText ="This Text Will Change";
+
     }
 }
 
+function changeF() {
+    if (currentWordIndex < text.length) {
+        document.getElementById("change").innerText = text[currentWordIndex];
+        currentWordIndex++;
+    } else {
+        currentWordIndex = 0;
+        clearInterval(IntervalID);
+        document.getElementById("change").innerText ="This Text Will Change";
+    }
+}
